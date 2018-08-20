44a36ce7896cbd485cca349191796c441eb1f9ea
Anggit <anggitbaskoro7@gmail.com>
2018-08-20 20:10:15 +0700

    unsuccessful edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    # Handle a succesfull update
+
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
