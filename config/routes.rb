8ac80e2ecfdabddeff6ee835d25731dde92c7768
Anggit <anggitbaskoro7@gmail.com>
2018-08-21 16:58:28 +0700

    activations controller

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   post '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 
   # root 'static_pages#home'
   # get 'static_pages/help'
