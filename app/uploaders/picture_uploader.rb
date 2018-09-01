0d0a22345d119aad9fc79928665d9656010d4ccb
Anggit <anggitbaskoro7@gmail.com>
2018-09-01 16:20:26 +0700

    finish microposts

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,14 +5,14 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   process resize_to_limit: [400,400]
   # Choose what kind of storage to use for this uploader:
-  # storage :file  # untuk local
+  storage :file  # untuk local
   # storage :fog   # untuk cloud storage
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
