2047789546fd772d2121426d3b5bf0b7df725122
Anggit <anggitbaskoro7@gmail.com>
2018-09-05 10:41:41 +0700

    following test

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,8 +3,8 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
     respond_to do |format|
       format.html {redirect_to @user}
       format.js
@@ -12,8 +12,8 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
     # redirect_to user (versi asli sebelum working follow button with ajax)
     respond_to do |format|
       format.html { redirect_to @user }
--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -11,7 +11,7 @@ class RelationshipsControllerTest < ActionDispatch::IntegrationTest
 
   test "destroy should require logged-in user" do
     assert_no_difference 'Relationship.count' do
-      delete.relationships_path(relationships(:one))
+      delete relationship_path(relationships(:one))
     end
     assert_redirected_to login_url
   end
